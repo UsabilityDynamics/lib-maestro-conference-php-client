22e4a83c6bdca6afe8d07c2f09e18d68b61940e6
pemaxim <maxim.peshkov@usabilitydynamics.com>
2014-11-27 17:32:27 +0200

    added composer.json and main client class

---
+++ lib/classes/class-client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Client
+ *
+ * @since 0.1.0
+ */
+namespace UsabilityDynamics\MC {
+
+  if( !class_exists( 'UsabilityDynamics\MC\Client' ) ) {
+
+    class Bootstrap extends \UsabilityDynamics\MC\Client {
+      
+      
+      
+    }
+
+  }
+
+}
